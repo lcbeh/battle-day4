5893af7cb691925ea75f0e03c06a781d3ffea600
Katy McCann <katy.alice.mccann@gmail.com>
2016-10-12 17:24:20 +0100

    Add hit point information

--- spec/name_spec.rb
+++ spec/name_spec.rb
@@ -12,3 +12,13 @@ feature 'Start' do
     expect(page).to have_content('Li vs. Katy')
   end
 end
+
+feature 'Hit point information' do
+  scenario "allow player1 to see player2's hit points" do
+    visit "/"
+    fill_in :player1, with: 'Li'
+    fill_in :player2, with: 'Katy'
+    click_button "Submit"
+    expect(page).to have_content("Katy's hit points: 60/60")
+  end
+end
