c9b92a414f111b2126dd048b1e6e7a056e85ebe9
lcbeh <lichien2010@yahoo.co.uk>
2016-10-13 15:35:37 +0100

    skinny controller

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,19 @@
 class Game
 
-def attack(opponent)
-  opponent.receive_damage
-end
+  def initialize (player1, player2)
+    @players = [player1, player2]
+  end
+
+  def player1
+    @players.first
+  end
+
+  def player2
+    @players.last
+  end
+
+  def attack(opponent)
+    opponent.receive_damage
+  end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,9 +5,13 @@ describe Game do
 
   player_1 = Player.new("Kyaw")
   player_2 = Player.new("Li")
-  subject(:game) { described_class.new }
+  subject(:game) { described_class.new(player_1, player_2) }
 
-  it "damages the player", :focus do
+  it "accepts two players" do
+    expect(game).to have_attributes( player1: player_1, player2: player_2)
+  end
+
+  it "damages the player" do
     expect(player_2).to receive(:receive_damage)
     game.attack(player_2)
   end
