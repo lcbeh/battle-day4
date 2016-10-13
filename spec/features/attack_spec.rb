18208faf55d0dd21594ebd371961fc3886beeda6
lcbeh <lichien2010@yahoo.co.uk>
2016-10-13 10:33:20 +0100

    Ex.22

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 
 feature 'Attack' do
   scenario 'Attack opponent and get confirmation' do
-    sign_in_and_play'
-    click_link('Attack')
+    sign_in_and_play
+    click_button('Attack')
     expect(page).to have_content("Li attacked Katy")
   end
 end
